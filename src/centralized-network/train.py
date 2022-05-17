698f9c13f640e06ca0be5bc6cf1e670640adee78
ulysse <ulysse.marquis@grenoble-inp.org>
2022-05-17 19:09:47 +0200

    removed early stopping+ added validation accuracy in the training loop, to be able to compare models

--- src/centralized-network/train.py
+++ src/centralized-network/train.py
@@ -6,13 +6,17 @@ import ray
 import numpy as np
 
 
-# tune : learning rate, weight decay, momentum optimizer
+# tune : learning rate, weight decay, momentum, optimizer
 
 # for HP tuning : https://pytorch.org/tutorials/beginner/hyperparameter_tuning_tutorial.html#the-train-function 
 
-def train(model, dataloader, n_epochs=10, batch_size=256):
+def train(model, trainloader, valloader, n_epochs=10, batch_size=256):
     """
     training of the global, centralized model
+
+    if the loss seems stagnant : early stopping
+    validation accuracy at each epoch
+    
     """
 
     model = ResNet()
@@ -20,7 +24,6 @@ def train(model, dataloader, n_epochs=10, batch_size=256):
 
     criterion = CrossEntropyLoss()
 
-    early_stopping = EarlyStopping()
     optimizer = optimizer(model.params())
     # criterrion
     # optimizer
@@ -41,11 +44,35 @@ def train(model, dataloader, n_epochs=10, batch_size=256):
             loss.backward()
             optimizer.step()
 
-        epoch_loss = np.mean(np.array(losses))
-        early_stopping(epoch_loss, model, i_epoch)
-        if early_stopping.early_stop:
-            print("Early stopping")
-            break
+        
+        
+        val_loss = 0.
+        val_steps = 0
+        total = 0
+        correct = 0
+
+        for i,data in enumerate(valloader):
+            with torch.no_grad():
+                imgs, labels = data
+                imgs, labels = imgs.cuda(), labels.cuda()
+
+                out = model(img)
+                _, predicted = torch.max(out.data, 1) # out : probabilities, we ignore the indexes
+
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+                loss = criterion(outputs, labels)
+                val_loss += loss.cpu().numpy()
+                val_steps += 1
+
+        with tune.checkpoint_dir(epoch) as checkpoint_dir:
+            path = os.path.join(checkpoint_dir, "checkpoint")
+            torch.save((model.state_dict(), optimizer.state_dict()), path)
+
+        tune.report(loss=(val_loss / val_steps), accuracy=correct / total)
+
+        
         
     #model.load_state_dict(torch.load(early_stopping.path))
     return model
--- src/data/provider.py
+++ src/data/provider.py
@@ -3,13 +3,39 @@ import torch
 
 # functions for getting the data
 
+DATAPATH = '.cifar10'
+
+def get_train_validation_data(transform, train_proportion = 0.8, BATCH_SIZE=256, shuffle=True, n_worker=8):
+    dataset = torchvision.datasets.CIFAR10(
+            DATAPATH, train=True, download=True, 
+            transform=transform,
+            num_workers=n_worker
+        )
+    test_abs = int(len(dataset) * train_proportion)
+
+    train_subset, val_subset = random_split(
+        dataset, [test_abs, len(trainset) - test_abs])
+
+    trainloader = torch.utils.data.DataLoader(
+        train_subset,
+        batch_size=int(config["batch_size"]),
+        shuffle=True,
+        num_workers=8)
+    valloader = torch.utils.data.DataLoader(
+        val_subset,
+        batch_size=int(config["batch_size"]),
+        shuffle=True,
+        num_workers=8)
+
+    return trainloader, valloader
+
 def get_training_data(transform, BATCH_SIZE=256,shuffle=True, n_worker=8):
     """
     get DataLoader for training data
     """
     trainloader = torch.utils.data.DataLoader(
         torchvision.datasets.CIFAR10(
-            '.cifar10', train=True, download=True, 
+            DATAPATH, train=True, download=True, 
             transform=transform,
             num_workers=n_worker
         ),
@@ -24,7 +50,7 @@ def get_testing_data(transform, BATCH_SIZE=256, shuffle=True, n_worker=8):
 
     testloader = torch.utils.data.DataLoader(
         torchvision.datasets.CIFAR10(
-            '.cifar10', train=False, download=True, 
+            DATAPATH, train=False, download=True, 
         ),
         batch_size=BATCH_SIZE, shuffle=shuffle,
         transform=transform,
