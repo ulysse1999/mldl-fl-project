b4252ab3a28e0a9f7507ea638c9fd759e330da1d
ulysse <ulysse.marquis@grenoble-inp.org>
2022-05-17 10:48:34 +0200

    use torchinfo instead of torchsummary

--- src/centralized-network/network-test.py
+++ src/centralized-network/network-test.py
@@ -1,6 +1,6 @@
 import torch
 from resnet50 import ResNet
-from torchsummary import summary
+from torchinfo import summary
 
 resnet = ResNet()
 resnet.to(torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
