e10a2a7a5a955eebf3117024dfb6288a2b133554
ulysse <ulysse.marquis@grenoble-inp.org>
2022-05-17 12:23:25 +0200

    add transform parameter

--- src/data/provider.py
+++ src/data/provider.py
@@ -1,23 +1,32 @@
 import torchvision
 import torch
 
+# functions for getting the data
 
-def get_training_data(BATCH_SIZE=256,shuffle=True):
+def get_training_data(transform, BATCH_SIZE=256,shuffle=True):
+    """
+    get DataLoader for training data
+    """
     trainloader = torch.utils.data.DataLoader(
         torchvision.datasets.CIFAR10(
             '.cifar10', train=True, download=True, 
+            transform=transform
         ),
         batch_size=BATCH_SIZE, shuffle=shuffle
     )
     return trainloader
 
-def get_testing_data(BATCH_SIZE=256, shuffle=True):
+def get_testing_data(transform, BATCH_SIZE=256, shuffle=True):
+    """
+    get DataLoader for testing data
+    """
 
     testloader = torch.utils.data.DataLoader(
         torchvision.datasets.CIFAR10(
             '.cifar10', train=False, download=True, 
         ),
-        batch_size=BATCH_SIZE, shuffle=shuffle
+        batch_size=BATCH_SIZE, shuffle=shuffle,
+        transform=transform
     )
 
     return testloader
