c8d06d1bf565135868829c3122e4390d554d5eb0
ulysse <ulysse.marquis@grenoble-inp.org>
2022-05-18 16:40:35 +0200

    typo randomhorizontalsplit

--- src/centralized-network/data/augmentation.py
+++ src/centralized-network/data/augmentation.py
@@ -1,11 +1,11 @@
-from torchvision.transforms import Compose, ToTensor, Normalize, RandomHorizontalSplit, RandomCrop
+from torchvision.transforms import Compose, ToTensor, Normalize, RandomHorizontalFlip, RandomCrop
 
 
 def get_transform(p_flip=0.5):
     transform = Compose(
         [   
             RandomCrop(size=32)
-            RandomHorizontalSplit(p_flip),
+            RandomHorizontalFlip(p_flip),
             ToTensor(),
             Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
