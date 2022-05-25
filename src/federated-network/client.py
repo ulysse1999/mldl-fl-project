35949c286cfa86ead7b538bc191425491d16a731
ulysse <ulysse.marquis@grenoble-inp.org>
2022-05-25 16:32:23 +0200

    client training

--- src/federated-network/client.py
+++ src/federated-network/client.py
@@ -1,8 +1,43 @@
+from resnet50 import ResNet
+import torch, torch.nn
+import torchvision
+from torch.optim import SGD
+from torch.nn import CrossEntropyLoss
+
+
 class Client:
 
-    def __init__(self,)
+    def __init__(self, normalization, local_dataset, epochs=1):
+        self.model = ResNet(normalization)
+        self.optimizer = SGD(lr=1)
+        self.dataset = torch.utils.data.DataLoader(
+            local_dataset,
+            BATCH_SIZE=32,
+            shuffle=True,
+            num_workers=2,
+            pin_memory=True
+            )
+        self.epochs=epochs
 
-    def train():
+
+    def train(self):
+
+        criterion = CrossEntropyLoss()
+        criterion.cuda()
+
+        for epoch in self.epochs:
+            # training loop
+            for i, data in enumerate(self.dataset):
+                imgs, labels = data
+                imgs, labels = imgs.cuda(), labels.cuda()
+
+                self.optimizer.zero_grad()
+                pred = self.model(imgs)
+                pred = pred.cuda()
+                
+                loss = criterion(pred, labels)
+                loss.backward()
+                self.optimizer.step()
 
     def send_data():
 
--- src/federated-network/main.py
+++ src/federated-network/main.py
@@ -1,19 +1,49 @@
-from data.provider import get_dataset, get_iid_split
+from data.provider import get_dataset, get_iid_split, N_CLIENTS, N_IMAGES_PER_CLIENT
 from data.augmentation import get_transform
+from client import Client
+from server import Server
+from argparse import ArgumentParser
 
 
-def main():
+# global parameters : number of epochs locally, normalization type
 
+
+
+def main(epochs, normalization, rounds):
+
+    # get data and split it
     transform = get_transform()
     dataset = get_dataset(transform)
     subdatasets = get_iid_split(dataset)
 
+    # create clients
 
+    clients = dict()
 
-    # split the data
+    for i in range(N_CLIENTS):
+        clients[i] = Client(subdatasets[i])
 
-    # create clients and server
+    # create server
 
-    # send data to the clients
+    server = Server(normalization)
+
 
     # training loop
+
+    for _ in range(rounds):
+        ...
+
+
+if __name__=='__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument("--normalization", type=str, choices=["group", "batch"], required=True)
+    parser.add_argument("--epochs", type=int, required=True)
+    parser.add_argument("--rounds", type=int, required=True)
+    parser.add_argument("--batchsize", type=int, required=True)
+
+    args = parser.parse_args()
+    main(args.epochs, args.normalization, args.rounds)
+
+
+
