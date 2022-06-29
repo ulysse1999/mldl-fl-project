6b722c3fb56d254c520002f3beeb9ffe1b1e662c
ricanicida <ricakazama@gmail.com>
2022-06-29 16:29:02 +0200

    debug

--- src/fedgkt/server.py
+++ src/fedgkt/server.py
@@ -61,7 +61,7 @@ class Server:
 
         for epoch in range(self.epochs):
             # training loop
-            print(client_learnings.shape)
+            print(dir(client_learnings))
             for i, data in enumerate(client_learnings):
 
                 imgs, labels = data
