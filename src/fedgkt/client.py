793f700663e9ac7ba3baeca5a1116ca1c3b6ce11
ricanicida <ricakazama@gmail.com>
2022-06-29 16:37:31 +0200

    add KLDivLoss

--- src/fedgkt/client.py
+++ src/fedgkt/client.py
@@ -64,7 +64,7 @@ class Client:
                 pred, feats = self.model(imgs)
                 pred = pred.cuda()
 
-                loss = crossEntropy(pred, labels) # +KLDiv(pred, labels)
+                loss = sum(crossEntropy(pred, labels),KLDiv(pred, labels))
                 loss.backward()
                 optimizer.step()
 
@@ -83,7 +83,7 @@ class Client:
             
             pred = pred.cuda()
 
-            loss = crossEntropy(pred, labels) # +KLDiv(pred, labels)
+            loss = sum(crossEntropy(pred, labels),KLDiv(pred, labels))
             loss.backward()
             optimizer.step()
 
