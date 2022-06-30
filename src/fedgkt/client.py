051e2503ad8235c9a62abdf714e4cb88890b654a
ricanicida <ricakazama@gmail.com>
2022-06-30 15:22:53 +0200

    debug

--- src/fedgkt/client.py
+++ src/fedgkt/client.py
@@ -64,6 +64,7 @@ class Client:
                 pred, feats = self.model(imgs)
                 pred = pred.cuda()
 
+                print(type(pred))
                 loss = sum([crossEntropy(pred, labels), KLDiv(pred, labels)])
                 loss.backward()
                 optimizer.step()
