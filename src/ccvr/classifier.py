6a7bab575c810eec9e372166bc755291b2091ef8
ulysse <ulysse.marquis@grenoble-inp.org>
2022-07-03 18:10:26 +0200

    classifier initialization

--- src/ccvr/classifier.py
+++ src/ccvr/classifier.py
@@ -3,6 +3,7 @@ import torch, torch.nn as nn
 class Classifier(nn.Module):
 
     def __init__(self):
+        super().__init__()
         self.fc = nn.Linear(1 << 11, 10)
 
     def forward(self, x):
