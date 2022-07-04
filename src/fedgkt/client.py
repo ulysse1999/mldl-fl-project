f61b49c714e3973489a45c4088040ba06f46c18b
ricanicida <ricakazama@gmail.com>
2022-07-04 23:25:06 +0200

    debug

--- src/fedgkt/client.py
+++ src/fedgkt/client.py
@@ -104,7 +104,7 @@ class Client:
         self.model_dict = self.model.state_dict()
         torch.cuda.empty_cache()
 
-        learnings = TensorDataset(torch.Tensor(feats_list), torch.Tensor(pred_list))
+        learnings = TensorDataset(feats_list, pred_list)
 
         print(f"features list: {torch.cat(feats_list).size()}")
 
